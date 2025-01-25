475c7c09ea01bd8ac6e7af86d22a13d3726eb0cf
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-25 12:07:50 -0500

    doing bubble things

--- Assets/Scripts/BubbleSpawner.cs
+++ Assets/Scripts/BubbleSpawner.cs
@@ -1,17 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
-// using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class BubbleSpawner : MonoBehaviour
 {
     public Canvas canvas;
+    public int poolAmount = 5;
+
+    [SerializeField] private GameObject bubbleContainer;
+    [SerializeField] private Button bubbleAsset;
+    private GameObject[] bubblePool;
 
     void Start()
     {
-        
+        bubblePool = new GameObject[poolAmount];
+        for (int i = 0; i < bubblePool.Length; i++) {
+            int currIndex = i;
+            bubblePool[i] = Instantiate(bubbleAsset.gameObject, bubbleContainer.transform);
+            bubblePool[i].GetComponent<Button>().onClick.AddListener(() => {
+                ButtonClicked(currIndex);
+            });
+
+            // set bubble position somewhere leftside offscreen
+        }
     }
 
     void Update()
     {
+        // loop through the bubble pool
+
         
+    }
+
+    private void ButtonClicked(int idx) {
+        // If a bubble is cllicked, make disappear
+        bubblePool[idx].SetActive(false);
+
+        // set it back to the original position
+
     }
 }
