f9eae4c6bf4ac3770f55929fb95c338ef30ef507
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-25 17:18:52 -0500

    gameobjects moveable

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -1,23 +1,30 @@
+using System;
 using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
+    [SerializeField] private GameObject[] parallaxElements;
+    private Vector3[] startPositions;
+
+    private float mm = 200f;
     private Vector2 mPos;
 
     void Start()
     {
-        LogMousePosition();
+        startPositions = new Vector3[parallaxElements.Length];
+        for (int i = 0; i < startPositions.Length; i++) {
+            startPositions[i] = parallaxElements[i].transform.position;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            LogMousePosition();
+        for (int i = 0; i < parallaxElements.Length; i++) {
+            GameObject background = parallaxElements[i];
+            mPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            float posX = Mathf.Lerp(background.transform.position.x, startPositions[i].x + (mPos.x - 0.5f) * (mm / (i + 1)), Time.deltaTime);
+            float posY = Mathf.Lerp(background.transform.position.y, startPositions[i].y + (mPos.y - 0.5f) * (mm / (i + 1)), Time.deltaTime);
+            background.transform.position = new(posX, posY);
         }
-    }
-
-    private void LogMousePosition() {
-        mPos = Input.mousePosition;
-        Debug.Log(mPos);
     }
 }
