bc1092ee12a7529568409494f858cb438697a44b
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-24 23:20:46 -0500

    added script

---
+++ Assets/Scripts/BubbleSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+// using UnityEngine.UI;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    public Canvas canvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
