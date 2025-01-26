449d401267f48b907b64f4dc260a1ce51c2e91ea
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-26 11:43:44 -0500

    add function calls for life force

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -3,4 +3,8 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     public int lifeForce = 10;
+
+    public void DecrementLifeForce() => lifeForce--;
+
+    public void IncrementLifeForce() => lifeForce++;
 }
