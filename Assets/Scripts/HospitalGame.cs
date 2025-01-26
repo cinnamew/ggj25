4516d9c3303ee421f22f1d3d3ee28bfceda4ca42
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-25 22:39:25 -0500

    hospital game functional

--- Assets/Scripts/HospitalGame.cs
+++ Assets/Scripts/HospitalGame.cs
@@ -1,20 +1,40 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HospitalGame : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI lfText;
     [SerializeField] private Slider otherLife;
     private readonly float multiplier = 0.2f;
     private readonly float addition = 0.15f;
 
+    private bool gameActive = true;
+
+
+    private void Start() 
+    {
+        lfText.text = "Life Force: " + GameManager.Instance.lifeForce;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            otherLife.value += addition;
-            GameManager.Instance.lifeForce--;
+        if (gameActive) {
+            if (Input.GetKeyDown(KeyCode.Space) && GameManager.Instance.lifeForce > 0) {
+                otherLife.value += addition;
+                GameManager.Instance.lifeForce--;
+                lfText.text = "Life Force: " + GameManager.Instance.lifeForce;
+            }
+            else {
+                otherLife.value -= Time.deltaTime * multiplier;
+            }
+            if (otherLife.value <= 0) {
+                gameActive = false;
+            }
         }
-        else {
-            otherLife.value -= Time.deltaTime * multiplier;
-        }
+        
+        Debug.Log("Game Done");
+        // Change scene or do something here when lose
+        // ... //
     }
 }
