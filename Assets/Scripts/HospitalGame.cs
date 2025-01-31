66f039d8e675e8441e60d6ac1865754627723db3
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-01-30 19:38:59 -0500

    hospital proof of concept functional

--- Assets/Scripts/HospitalGame.cs
+++ Assets/Scripts/HospitalGame.cs
@@ -1,40 +1,39 @@
-using TMPro;
+using Fungus;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HospitalGame : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI lfText;
+    [Header("Fungus Settings")]
+    [SerializeField] private Flowchart flowchart;
+    [SerializeField] private string failBlockName;
+
+    [Header("Life Bar Settings")]
     [SerializeField] private Slider otherLife;
-    private readonly float multiplier = 0.2f;
-    private readonly float addition = 0.15f;
+    [SerializeField] private float multiplier = 0.2f;
+    [SerializeField] private float addition = 0.15f;
 
     private bool gameActive = true;
 
-
-    private void Start() 
-    {
-        lfText.text = "Life Force: " + GameManager.Instance.lifeForce;
-    }
-
     private void Update()
     {
-        if (gameActive) {
-            if (Input.GetKeyDown(KeyCode.Space) && GameManager.Instance.lifeForce > 0) {
+        if (gameActive) 
+        {
+            if (Input.GetKeyDown(KeyCode.J) && LifeForceManager.Instance.lifeForceAmount > 0) 
+            {
                 otherLife.value += addition;
-                GameManager.Instance.lifeForce--;
-                lfText.text = "Life Force: " + GameManager.Instance.lifeForce;
+                LifeForceManager.Instance.DecrementLifeForceAmount();
             }
-            else {
+            else 
+            {
                 otherLife.value -= Time.deltaTime * multiplier;
             }
-            if (otherLife.value <= 0) {
+            if (otherLife.value <= 0) 
+            {
+                flowchart.StopBlock(flowchart.SelectedBlock.BlockName);
+                flowchart.ExecuteBlock(failBlockName);
                 gameActive = false;
             }
         }
-        
-        Debug.Log("Game Done");
-        // Change scene or do something here when lose
-        // ... //
     }
 }
