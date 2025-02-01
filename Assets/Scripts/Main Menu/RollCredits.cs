c65bc9245b48d9cbf10b90a95f9288f8afd2b0b8
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-02-01 18:36:18 -0500

    updated references for credits script

--- Assets/Scripts/Main Menu/RollCredits.cs
+++ Assets/Scripts/Main Menu/RollCredits.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RollCredits : MonoBehaviour
 {
+    [SerializeField] private Image titleImage;
+    [SerializeField] private RectTransform scrollingContainer;
     [SerializeField] private float scrollingSpeed = 200f;
     private float delayInSeconds = 2f;
     private float timer = 0f;
 
-    void Update()
+    private void Update()
     {
         timer += Time.deltaTime;
-        if (timer > delayInSeconds) {
-            transform.position += scrollingSpeed * Time.deltaTime * Vector3.up;
+        titleImage.color = new Color(titleImage.color.r, titleImage.color.g, titleImage.color.b, Mathf.Lerp(0, 1, timer));
+        if (timer > delayInSeconds) 
+        {
+            scrollingContainer.position += scrollingSpeed * Time.deltaTime * Vector3.up;
         }
     }
 }
