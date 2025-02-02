5f1c5531841775fc15acbea8b07ff5c704aacb3b
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-02-01 21:43:48 -0500

    added skipping
    bruh who skips the credits anyway lmao

--- Assets/Scripts/Main Menu/RollCredits.cs
+++ Assets/Scripts/Main Menu/RollCredits.cs
@@ -1,21 +1,65 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RollCredits : MonoBehaviour
 {
-    [SerializeField] private Image titleImage;
+    [SerializeField] private Image fade;
     [SerializeField] private RectTransform scrollingContainer;
     [SerializeField] private float scrollingSpeed = 200f;
+    [SerializeField] private RectTransform[] cardBubbles;
     private float delayInSeconds = 2f;
     private float timer = 0f;
+    private bool scrolling = false;
+    private int currentIndex;
+    private int fadeFrom = 1;
+    private int fadeTo = 0;
+    private bool finished = false;
+
+    private void Start() 
+    {
+        timer -= delayInSeconds;
+    }
 
     private void Update()
     {
+        // Timer related stuff
         timer += Time.deltaTime;
-        titleImage.color = new Color(titleImage.color.r, titleImage.color.g, titleImage.color.b, Mathf.Lerp(0, 1, timer));
-        if (timer > delayInSeconds) 
+        if (timer > delayInSeconds) scrolling = true;
+
+        // Title image fading in
+        fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, Mathf.Lerp(fadeFrom, fadeTo, timer));
+
+        // End early - Skip credits (Who in their right mind would skip the credits scene???)
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            timer = 0;
+            fadeFrom = 0;
+            fadeTo = 1;
+            finished = true;
+        }
+
+        // Final message at the end
+        if (scrollingContainer.localPosition.y >= 4500) 
         {
-            scrollingContainer.position += scrollingSpeed * Time.deltaTime * Vector3.up;
+            scrolling = false;
+            finished = true;
+            timer = 0;
+        }
+
+        // Actual credits scrolling
+        if (scrolling) scrollingContainer.position += scrollingSpeed * Time.deltaTime * Vector3.up;
+
+        // Pop the funny bubbles - waiting for the pop frame
+        if (currentIndex < cardBubbles.Length && cardBubbles[currentIndex].position.y >= 350) {
+            Debug.Log("Pop for bubble named " + cardBubbles[currentIndex].name);
+            if (currentIndex == 3) {
+                cardBubbles[currentIndex].gameObject.SetActive(false);
+            }
+            currentIndex++;
+        }
+        
+        if (finished && timer >= 1f) {
+            SceneManager.LoadScene("Main Menu");
         }
     }
 }
