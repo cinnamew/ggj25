381dd8ce77adc20fc2a5da2698a3970635c9e11c
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-02-08 21:22:36 -0500

    settings windows functional

--- Assets/Scripts/Main Menu/MenuButton.cs
+++ Assets/Scripts/Main Menu/MenuButton.cs
@@ -5,7 +5,6 @@ public class MenuButton : MonoBehaviour
 {
     public void ReturnToMenu()
     {
-        Destroy(transform.parent.parent.parent.gameObject);
         SceneManager.LoadScene("Main Menu");
     }
 }
