333fe23e03c917bf2e393102302d63289cc0da34
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2025-02-09 11:51:16 -0500

    blocker and ignoredialog fixes

--- Assets/Scripts/SideButtons.cs
+++ Assets/Scripts/SideButtons.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class SideButtons : MonoBehaviour
 {
+    [SerializeField] CanvasGroup history;
     [SerializeField] GameObject settings;
-    [SerializeField] GameObject fakePanel;
+    [SerializeField] GameObject blocker;
 
-    public void ToggleSettingsWindow() => settings.SetActive(!settings.activeSelf);
-    public void ToggleFakePanel() => fakePanel.SetActive(!fakePanel.activeSelf);
+    public void ToggleSettingsWindow()
+    {
+        settings.SetActive(!settings.activeSelf);
+    }
+
+    public void ToggleFakePanel()
+    {
+        if (blocker.activeSelf && (history.blocksRaycasts == true || settings.activeSelf)) {
+            return;
+        }
+        blocker.SetActive(!blocker.activeSelf);
+        GetComponent<IgnoreDialogueClick>().ChangeDialogInputClickMode(blocker.activeSelf ? Fungus.ClickMode.Disabled : Fungus.ClickMode.ClickAnywhere);
+        // Debug.Log();
+    }
 }
